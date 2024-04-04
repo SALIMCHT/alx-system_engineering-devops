48efb03cb5693121f6c14f1b34b311a6d8e0c55a
SALIMCHT <saliimchatbii@gmail.com>
2024-04-04 10:34:30 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,25 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Using Puppet, install flask from pip3
+
+# Ensure a specific version of Python 3 is installed
+package { 'python3':
+  ensure   => '3.8.10',
+  provider => 'pip3'
 }
+
+# Ensure a specific version of Werkzeug is installed
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+# Ensure that required development packages are installed
+package { ['libffi-dev', 'libssl-dev', 'python3-dev']:
+  ensure => installed,
+}
+
+# Execute a command to install Flask using pip3, version 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
